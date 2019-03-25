8eba197a7bd0e65f1c5054d25636db50af2d33e3
Daniel Forbes <dkforbes@hotmail.com>
2019-03-24 22:18:51 -0400

    Created Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World from Daniel");
+        }
+    }
+}
